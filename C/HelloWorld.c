788889aea7560c2eaf5b78de5dfbea616ef67b5c
Pascal Kiser <PascalKiser@photometer.com>
2018-09-25 14:02:59 +0200

    Freiwillige Aufgabe SW01

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -1,4 +1,17 @@
+/**
+ * HSLU HS18 MC
+ * Übung 0: Hello World 
+ * 
+ * Beispielprogramm
+ * 
+ * @author Pascal Kiser
+ * @date 25.06.2018
+ * @version 1.0
+ */
 #include <stdio.h>
+/**
+ * Gibt "Hello World" aus
+ */
 
 int main (int argc, char **argv) {
     printf("Hello World!\n");
